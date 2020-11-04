0c03cd55d6a3b0ca2b8f3199bdab13dc6660343f
Shane <dsishanew@gmail.com>
2020-11-03 19:19:54 -0500

    IMPORTANT UPDATE: Information and Instructions are in the "IMPORTANT" file.

---
+++ IMPORTANT.py
@@ -0,0 +1,14 @@
+# the "MP4.py" and "MP3.py" is not working. It was taken down due to copy rights claims.
+# Luckily there is a way to download youtube videos and music from youtube
+# navigate to where "youtube-dl.exe", "ffmpeg.exe", "ffplay.exe", and "ffprobe.exe" are installed.
+# ^^ Modules named in the previous line is attached to the Github project. ^^
+
+# open cmd where those modules are installed
+# type:
+    # to download just the auido:
+        # type; "youtube-dl --audio-format mp3 --audio-quality 6 -x" + the link to the youtube video
+    # to download the video + audio:
+        # type; "youtube-dl" + the link to the youtube video
+
+
+

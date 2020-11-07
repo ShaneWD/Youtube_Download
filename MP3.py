06c1000607fdf6c8f0bc6ca1706cdafed3e04d76
Shane <dsishanew@gmail.com>
2020-11-06 20:00:47 -0500

    slight polish to code

--- MP3.py
+++ MP3.py
@@ -1,32 +1,15 @@
 from __future__ import unicode_literals
 import os
+
+
 def c_mp3():
     link = input("""
 Insert link
 >""")
-    # (test link) link = "https://www.youtube.com/watch?v=JQbjS0_ZfJ0"
     try:
-        # os.system(f'cmd /k youtube-dl --audio-format mp3 -f bestaudio -x {link}')
-        # # os.system(f'cmd /k youtube-dl -x --audio-quality 0 --audio-format mp3 {link}')
-        # os.system(f'cmd /k youtube-dl --audio-format mp3 --audio-quality 0 {link}')
-        # ignore; os.system(f'cmd /k youtube-dl -f bestaudio -x {link}')
         os.system(f'cmd/k youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 {link}')
     except:
         print("invalid link")
-
-#ydl_opts = {
-    #'format': 'bestaudio/best',
-    #'postprocessors': [{
-        #'key': 'FFmpegExtractAudio',
-        #'preferredcodec': 'mp3',
-        #'preferredquality': '320',
-    #}],
-#}
-
-#with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    #ydl.download([link])
-
-# credit: https://github.com/silvanohirtie/youtube-mp3-downloader
 
 # IMPORTANT: This DOES NOT work without "ffmpeg", "ffplay", and "ffprobe". Make sure you download these files and put
 # in our scripts folder (where pip is). These three important files will be attached to this github.
--- MP4.py
+++ MP4.py
@@ -17,7 +17,6 @@ Remember, the new name CANNOT contain any spaces
             new_name = original_name
         os.system(f'ffmpeg -i {original_name} -vcodec copy {new_name}')
 
-
     url = input("""
 Insert the URL
 or 
@@ -38,21 +37,3 @@ Yes or No?""").lower()
             print("invalid link")
     elif url.lower() == "convert":
         convert()
-
-#ydl_opts = {}
-#with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    #ydl.download([url])
-
-# To convert the mkv file to mp4:
-    # Type this into the command prompt/terminal
-    # ffmpeg -i 123.mkv -vcodec copy 1.mp4
-    # the 123.mkv should be replaced with the file name
-    # 1.mp4 should be replaced with what ever you want the new file to be called
-
-
-# IMPORTANT: This DOES NOT work without "ffmpeg", "ffplay", and "ffprobe". Make sure you download these files and put
-# in our scripts folder (where pip is). These three important files will be attached to this github.
-
-# more information on the gitgub
-# test
-# https://github.com/ShaneWD/Youtube_Download
